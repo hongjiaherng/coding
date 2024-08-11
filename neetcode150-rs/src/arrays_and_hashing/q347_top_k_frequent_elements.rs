8c8059039ceccea2d0e6b6d00f189366ee1047aa
hongjiaherng <jiaherng2001@gmail.com>
2024-08-11 20:51:48 +0800

    q347

--- neetcode150-rs/src/arrays_and_hashing/q347_top_k_frequent_elements.rs
+++ neetcode150-rs/src/arrays_and_hashing/q347_top_k_frequent_elements.rs
@@ -1,8 +1,32 @@
+use std::collections::HashMap;
+
 pub struct Solution;
 
 impl Solution {
+    /// - T(n) = O(n)
+    /// - S(n) = O(n)
     pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
-        todo!()
+        let mut num_counts: HashMap<i32, usize> = HashMap::new();
+
+        for &n in &nums {
+            *num_counts.entry(n).or_default() += 1;
+        }
+
+        let mut buckets: Vec<Vec<i32>> = vec![Vec::new(); nums.len() + 1];
+        for (&n, &count) in &num_counts {
+            buckets[count - 1].push(n);
+        }
+
+        let mut res = Vec::with_capacity(k as usize);
+        for bucket in buckets.iter().rev() {
+            for &n in bucket {
+                res.push(n);
+                if res.len() == k as usize {
+                    return res;
+                }
+            }
+        }
+        res
     }
 }
 
