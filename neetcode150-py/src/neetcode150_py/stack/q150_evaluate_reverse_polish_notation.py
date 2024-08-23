82e4219e9d73cee322373f79a71e35c316b74e1f
hongjiaherng <jiaherng2001@gmail.com>
2024-08-24 01:44:42 +0800

    Add complexity analysis result

--- neetcode150-py/src/neetcode150_py/stack/q150_evaluate_reverse_polish_notation.py
+++ neetcode150-py/src/neetcode150_py/stack/q150_evaluate_reverse_polish_notation.py
@@ -3,6 +3,10 @@ from typing import List
 
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
+        """
+        T(n) = O(n)
+        S(n) = O(n)
+        """
         stack: List[int] = []
         for token in tokens:
             match token:
