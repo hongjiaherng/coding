3f6fecd3a39210d37884f32fc46874079dd4cc7e
hongjiaherng <jiaherng2001@gmail.com>
2025-08-04 21:07:22 +0800

    feat: q167

--- neetcode150-rs/src/two_pointers/q167_two_sum_ii_input_array_is_sorted.rs
+++ neetcode150-rs/src/two_pointers/q167_two_sum_ii_input_array_is_sorted.rs
@@ -2,7 +2,19 @@ pub struct Solution;
 
 impl Solution {
     pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
-        todo!();
+        let (mut left, mut right) = (0, numbers.len() - 1);
+
+        while left < right {
+            let sum = numbers[left] + numbers[right];
+            if sum == target {
+                return vec![(left + 1) as i32, (right + 1) as i32];
+            } else if sum < target {
+                left += 1;
+            } else {
+                right -= 1;
+            }
+        }
+        vec![]
     }
 }
 
@@ -41,5 +53,12 @@ mod tests {
         let numbers = vec![-1, 0];
         let target = -1;
         assert_eq!(Solution::two_sum(numbers, target), vec![1, 2]);
+    }
+
+    #[test]
+    fn test_two_sum_example_4() {
+        let numbers = vec![2, 7, 11, 15];
+        let target = 22;
+        assert_eq!(Solution::two_sum(numbers, target), vec![2, 4]);
     }
 }
