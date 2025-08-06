175287348a9853cb73922668c6420481974c66d3
hongjiaherng <jiaherng2001@gmail.com>
2025-08-06 21:22:28 +0800

    feat: q11

--- neetcode150-rs/src/two_pointers/q11_container_with_most_water.rs
+++ neetcode150-rs/src/two_pointers/q11_container_with_most_water.rs
@@ -1,8 +1,23 @@
+use std::cmp::{max, min};
+
 pub struct Solution;
 
 impl Solution {
     pub fn max_area(height: Vec<i32>) -> i32 {
-        todo!();
+        let mut max_area = 0;
+        let (mut left, mut right) = (0, height.len() - 1);
+
+        while left < right {
+            let area = min(height[left], height[right]) * (right - left) as i32;
+            max_area = max(max_area, area);
+            if height[left] <= height[right] {
+                left += 1;
+            } else {
+                right -= 1;
+            }
+        }
+
+        max_area
     }
 }
 
