39a629e1dfbd672a5516601e3503626374835774
hongjiaherng <jiaherng2001@gmail.com>
2025-08-22 04:05:11 +0800

    feat: q981

--- neetcode150-rs/src/binary_search/q981_time_based_key_value_store.rs
+++ neetcode150-rs/src/binary_search/q981_time_based_key_value_store.rs
@@ -1,20 +1,50 @@
-pub struct TimeMap {}
+use std::{cmp::Ordering, collections::HashMap};
+
+#[derive(Debug)]
+pub struct TimeMap {
+    map: HashMap<String, Vec<(i32, String)>>,
+}
 
 /**
  * `&self` means the method takes an immutable reference.
  * If you need a mutable reference, change it to `&mut self` instead.
  */
 impl TimeMap {
-    fn new() -> Self {
-        todo!();
+    pub fn new() -> Self {
+        Self {
+            map: HashMap::new(),
+        }
     }
 
-    fn set(&self, key: String, value: String, timestamp: i32) {
-        todo!();
+    pub fn set(&mut self, key: String, value: String, timestamp: i32) {
+        self.map
+            .entry(key)
+            .or_insert_with(Vec::new)
+            .push((timestamp, value));
     }
 
-    fn get(&self, key: String, timestamp: i32) -> String {
-        todo!();
+    pub fn get(&self, key: String, timestamp: i32) -> String {
+        match self.map.get(&key) {
+            Some(item) => {
+                let mut res: Option<&String> = None;
+                let (mut left, mut right) = (0, item.len() as i32 - 1);
+
+                while left <= right {
+                    let mid = left + (right - left) / 2;
+
+                    match item[mid as usize].0.cmp(&timestamp) {
+                        Ordering::Less | Ordering::Equal => {
+                            res = Some(&item[mid as usize].1);
+                            left = mid + 1;
+                        }
+                        Ordering::Greater => right = mid - 1,
+                    }
+                }
+
+                res.cloned().unwrap_or_default()
+            }
+            None => String::new(),
+        }
     }
 }
 
@@ -31,7 +61,7 @@ mod tests {
 
     #[test]
     fn test_time_map() {
-        let time_map = TimeMap::new();
+        let mut time_map = TimeMap::new();
         time_map.set("foo".to_string(), "bar".to_string(), 1);
         assert_eq!(time_map.get("foo".to_string(), 1), "bar".to_string());
         assert_eq!(time_map.get("foo".to_string(), 3), "bar".to_string());
