e019e7adb750eb8afbf2759b59c96cda885a26c4
hongjiaherng <jiaherng2001@gmail.com>
2025-08-31 22:16:20 +0800

    feat: q76

--- neetcode150-rs/src/sliding_window/q76_minimum_window_substring.rs
+++ neetcode150-rs/src/sliding_window/q76_minimum_window_substring.rs
@@ -1,9 +1,59 @@
-#![allow(unused)]
 pub struct Solution;
 
 impl Solution {
     pub fn min_window(s: String, t: String) -> String {
-        todo!();
+        if s.len() < t.len() {
+            return String::new();
+        }
+
+        let (mut count_t, mut count_s) = ([0usize; 128], [0usize; 128]);
+
+        let mut required = 0;
+        for &ch in t.as_bytes() {
+            if count_t[ch as usize] == 0 {
+                required += 1;
+            }
+            count_t[ch as usize] += 1;
+        }
+
+        let mut best_len = usize::MAX;
+        let mut best_start = 0;
+        let mut matches = 0;
+        let chars = s.as_bytes();
+        let mut l = 0;
+
+        for r in 0..chars.len() {
+            let idx_r = chars[r] as usize;
+            count_s[idx_r] += 1;
+
+            if count_s[idx_r] == count_t[idx_r] && count_t[idx_r] > 0 {
+                matches += 1;
+            }
+
+            // Everything in count_s is complete match or greater than those in count_t
+            while matches == required {
+                // Get the shortest matching substring
+                if best_len > r - l + 1 {
+                    best_len = r - l + 1;
+                    best_start = l;
+                }
+
+                let idx_l = chars[l] as usize;
+                count_s[idx_l] -= 1;
+                // If shifting window to left causes condition to not match
+                if count_s[idx_l] < count_t[idx_l] && count_t[idx_l] > 0 {
+                    matches -= 1;
+                }
+
+                l += 1;
+            }
+        }
+
+        if best_len == usize::MAX {
+            String::new()
+        } else {
+            String::from_utf8(chars[best_start..best_start + best_len].to_vec()).unwrap()
+        }
     }
 }
 
