3ea7b937b6f55137fdaeda044682e80167791f1b
hongjiaherng <jiaherng2001@gmail.com>
2025-09-04 01:27:32 +0800

    feat: q239

--- neetcode150-rs/src/sliding_window/q239_sliding_window_maximum.rs
+++ neetcode150-rs/src/sliding_window/q239_sliding_window_maximum.rs
@@ -1,9 +1,36 @@
-#![allow(unused)]
+use std::collections::VecDeque;
+
 pub struct Solution;
 
 impl Solution {
     pub fn max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {
-        todo!();
+        let mut res: Vec<i32> = Vec::with_capacity(nums.len() - k as usize + 1);
+        let mut deque: VecDeque<usize> = VecDeque::new(); // Monotonically decreasing queue
+
+        for (i, &num) in nums.iter().enumerate() {
+            // Remove first element from queue if the element is out of range of the current window (l..=r)
+            if let Some(&front_i) = deque.front() {
+                if i >= front_i + k as usize {
+                    deque.pop_front();
+                }
+            }
+
+            // Attempt to add element to the back of the queue, remove any element that's less than the current element
+            while let Some(&back_i) = deque.back() {
+                if num > nums[back_i] {
+                    deque.pop_back();
+                } else {
+                    break;
+                }
+            }
+            deque.push_back(i);
+
+            // The first element is the max of the current window
+            if i + 1 >= k as usize {
+                res.push(nums[*deque.front().unwrap()]);
+            }
+        }
+        res
     }
 }
 
@@ -25,5 +52,19 @@ mod tests {
         let k = 1;
         let expected = vec![1];
         assert_eq!(Solution::max_sliding_window(nums, k), expected);
+    }
+
+    #[test]
+    fn test_max_sliding_window_example3() {
+        let nums = vec![1, -1];
+        let k = 1;
+        assert_eq!(Solution::max_sliding_window(nums, k), vec![1, -1]);
+    }
+
+    #[test]
+    fn test_max_sliding_window_example4() {
+        let nums = vec![9, 11];
+        let k = 2;
+        assert_eq!(Solution::max_sliding_window(nums, k), vec![11]);
     }
 }
