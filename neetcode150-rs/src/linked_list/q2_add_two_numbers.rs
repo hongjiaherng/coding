c2c7db24e8281ad662a3905956fa018047e0311a
hongjiaherng <jiaherng2001@gmail.com>
2025-09-07 20:14:07 +0800

    feat: q2

--- neetcode150-rs/src/linked_list/q2_add_two_numbers.rs
+++ neetcode150-rs/src/linked_list/q2_add_two_numbers.rs
@@ -1,4 +1,3 @@
-#![allow(unused)]
 use crate::linked_list::ListNode;
 
 pub struct Solution;
@@ -8,7 +7,42 @@ impl Solution {
         l1: Option<Box<ListNode>>,
         l2: Option<Box<ListNode>>,
     ) -> Option<Box<ListNode>> {
-        todo!()
+        // Iterate through l1 & l2 together
+        // For each node, add them up, create a new node with the value, save the carry value, use it on the next iteration
+        let mut dummy = Box::new(ListNode::new(-1));
+        let mut tail = &mut dummy;
+        let mut carry = 0;
+        let (mut curr1, mut curr2) = (l1, l2);
+
+        while curr1.is_some() || curr2.is_some() || carry > 0 {
+            let sum = match (curr1.as_mut(), curr2.as_mut()) {
+                (Some(n1), Some(n2)) => {
+                    let v = n1.val + n2.val;
+                    curr1 = n1.next.take();
+                    curr2 = n2.next.take();
+                    v
+                }
+                (Some(n1), None) => {
+                    let v = n1.val;
+                    curr1 = n1.next.take();
+                    v
+                }
+                (None, Some(n2)) => {
+                    let v = n2.val;
+                    curr2 = n2.next.take();
+                    v
+                }
+                (None, None) => 0,
+            } + carry;
+
+            carry = sum / 10;
+            let digit = sum % 10;
+
+            tail.next = Some(Box::new(ListNode::new(digit)));
+            tail = tail.next.as_mut().unwrap();
+        }
+
+        dummy.next
     }
 }
 
