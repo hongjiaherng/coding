b1c6adea23a648a684ba64820c6edc74ddc61fa0
hongjiaherng <jiaherng2001@gmail.com>
2025-09-26 17:54:51 +0800

    feat: q235

--- neetcode150-rs/src/trees/q235_lowest_common_ancestor_of_a_binary_search_tree.rs
+++ neetcode150-rs/src/trees/q235_lowest_common_ancestor_of_a_binary_search_tree.rs
@@ -1,4 +1,3 @@
-#![allow(unused)]
 use crate::trees::TreeNode;
 use std::cell::RefCell;
 use std::rc::Rc;
@@ -11,7 +10,25 @@ impl Solution {
         p: Option<Rc<RefCell<TreeNode>>>,
         q: Option<Rc<RefCell<TreeNode>>>,
     ) -> Option<Rc<RefCell<TreeNode>>> {
-        todo!()
+        let (p, q) = match (p, q) {
+            (Some(p), Some(q)) => (p, q),
+            _ => return None, // if either is None, no LCA
+        };
+        let p_val = p.borrow().val;
+        let q_val = q.borrow().val;
+        let mut cur = root;
+
+        while let Some(cur_rc) = cur {
+            let cur_val = cur_rc.borrow().val;
+            if p_val < cur_val && q_val < cur_val {
+                cur = cur_rc.borrow().left.clone();
+            } else if p_val > cur_val && q_val > cur_val {
+                cur = cur_rc.borrow().right.clone();
+            } else {
+                return Some(cur_rc);
+            }
+        }
+        None
     }
 }
 
@@ -41,8 +58,14 @@ mod tests {
         ]);
         let p = TreeNode::from_vec(vec![Some(2)]);
         let q = TreeNode::from_vec(vec![Some(8)]);
-        let expected = TreeNode::from_vec(vec![Some(6)]);
-        assert_eq!(Solution::lowest_common_ancestor(root, p, q), expected);
+        let expected = 6;
+        assert_eq!(
+            Solution::lowest_common_ancestor(root, p, q)
+                .unwrap()
+                .borrow()
+                .val,
+            expected
+        );
     }
 
     // Example 2:
@@ -66,8 +89,14 @@ mod tests {
         ]);
         let p = TreeNode::from_vec(vec![Some(2)]);
         let q = TreeNode::from_vec(vec![Some(4)]);
-        let expected = TreeNode::from_vec(vec![Some(2)]);
-        assert_eq!(Solution::lowest_common_ancestor(root, p, q), expected);
+        let expected = 2;
+        assert_eq!(
+            Solution::lowest_common_ancestor(root, p, q)
+                .unwrap()
+                .borrow()
+                .val,
+            expected
+        );
     }
 
     // Example 3:
@@ -78,7 +107,28 @@ mod tests {
         let root = TreeNode::from_vec(vec![Some(2), Some(1)]);
         let p = TreeNode::from_vec(vec![Some(2)]);
         let q = TreeNode::from_vec(vec![Some(1)]);
-        let expected = TreeNode::from_vec(vec![Some(2)]);
-        assert_eq!(Solution::lowest_common_ancestor(root, p, q), expected);
+        let expected = 2;
+        assert_eq!(
+            Solution::lowest_common_ancestor(root, p, q)
+                .unwrap()
+                .borrow()
+                .val,
+            expected
+        );
+    }
+
+    #[test]
+    fn test_lowest_common_ancestor_example_4() {
+        let root = TreeNode::from_vec(vec![Some(3), Some(1), Some(4), None, Some(2)]);
+        let p = TreeNode::from_vec(vec![Some(2)]);
+        let q = TreeNode::from_vec(vec![Some(3)]);
+        let expected = 3;
+        assert_eq!(
+            Solution::lowest_common_ancestor(root, p, q)
+                .unwrap()
+                .borrow()
+                .val,
+            expected
+        );
     }
 }
